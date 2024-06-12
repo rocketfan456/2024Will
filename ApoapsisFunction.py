32d1d19cd99a63946b57d727a375890e416754e8
WillSinc <147778490+WillSinc@users.noreply.github.com>
2024-06-12 16:29:41 -0400

    Update ApoapsisFunction.py
    Added function descriptions

--- ApoapsisFunction.py
+++ ApoapsisFunction.py
@@ -9,6 +9,23 @@ import numpy as np
 
 
 def visviva(mu,r,a):
+    '''
+    Calculate the velocity at point in orbit.
+
+    Parameters
+    ----------
+    mu : int
+        Gravitational Constant (km^2/s^2).
+    r : int
+        Distance from center body (km).
+    a : int
+        semi-major axis (km).
+
+    Returns
+    -------
+    v : float
+        velocity at specifies orbit point.
+    '''
     
     v = np.sqrt(mu*((2/r)-(1/a)))
     
@@ -18,6 +35,27 @@ def visviva(mu,r,a):
 
 
 def delVreq(mu,rF,rI,aF,aI):
+    '''
+    Calculate the difference in velocities at given points in orbit.
+    
+    Parameters
+    ----------
+    mu : int
+        Gravitational Constant (km^2/s^2).
+    rF : int
+        Final distance from center body (km).
+    aF : int
+        Final semi-major axis (km).
+    rI : int
+        Initial distance from center body (km).
+    aI : int
+        Initial semi-major axis (km).
+
+    Returns
+    -------
+    v : float
+        dV necessary to change from specified apogees.
+    '''
     
     delV = visviva(mu,rF,aF) - visviva(mu,rI,aI)
 
